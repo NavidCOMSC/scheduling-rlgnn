44a7c8a3b45648198a4b824bf4f80ff657df5b4a
NavidCOMSC <amir.navid.rahimi@gmail.com>
2025-08-06 10:35:19 +0100

    small edit of initializer

--- src/scheduling_rlgnn/__init__.py
+++ src/scheduling_rlgnn/__init__.py
@@ -3,5 +3,5 @@ Scheduling RLGNN: GNN architecture models with PyG and deep reinforcement learni
 """
 
 __version__ = "0.1.0"
-__author__ = "Navid"
+__author__ = "Navid Rahimi"
 __email__ = "amir.navid.rahimi@googlemail.com"
