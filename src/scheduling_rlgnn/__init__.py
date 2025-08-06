ad2994d92db01a80d8b49f97b83e092a64e9997d
NavidCOMSC <amir.navid.rahimi@gmail.com>
2025-08-06 10:30:26 +0100

    building the project structure from the source directory planned in the poetry environment

---
+++ src/scheduling_rlgnn/__init__.py
@@ -0,0 +1,7 @@
+"""
+Scheduling RLGNN: GNN architecture models with PyG and deep reinforcement learning.
+"""
+
+__version__ = "0.1.0"
+__author__ = "Navid"
+__email__ = "amir.navid.rahimi@googlemail.com"
