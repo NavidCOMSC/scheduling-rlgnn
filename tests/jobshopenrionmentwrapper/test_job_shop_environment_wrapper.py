b1fba874f0b494c1b9e81698b6530cece71e51aa
NavidCOMSC <amir.navid.rahimi@gmail.com>
2025-08-11 13:44:20 +0100

    function document is updated

--- tests/jobshopenrionmentwrapper/test_job_shop_environment_wrapper.py
+++ tests/jobshopenrionmentwrapper/test_job_shop_environment_wrapper.py
@@ -7,7 +7,8 @@ from scheduling_rlgnn.envwrapper import JobShopEnvironmentWrapper
 # Fixture for a minimal JobShopInstance
 @pytest.fixture
 def minimal_job_shop_instance():
-    """Creates a minimal JobShopInstance with 2 jobs and 2 machines."""
+    """Creates a minimal JobShopInstance with 2 jobs and
+    2 machines."""
     jobs = [
         [Operation(0, duration=3), Operation(1, duration=2)],
         [Operation(1, duration=2), Operation(0, duration=4)],
