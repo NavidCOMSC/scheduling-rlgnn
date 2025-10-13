2d3e71a5a761f7fb51ac763eca00e9e5f5a89d46
NavidCOMSC <amir.navid.rahimi@gmail.com>
2025-10-13 16:51:26 +0100

    exploration action distribution method is added

--- src/scheduling_rlgnn/rllib_agent/ppojobshoprlmodule.py
+++ src/scheduling_rlgnn/rllib_agent/ppojobshoprlmodule.py
@@ -232,3 +232,8 @@ class PPOJobShopRLModule(TorchRLModule):
     def get_train_action_dist_cls(self):
         """Return the action distribution class for training."""
         return TorchCategorical
+
+    @override(RLModule)
+    def get_exploration_action_dist_cls(self):
+        """Return the action distribution class for exploration."""
+        return TorchCategorical
