70bb7fcc4bff8bf0fddb373c39420808db0bcbd5
NavidCOMSC <amir.navid.rahimi@gmail.com>
2025-10-19 01:19:30 +0100

    module constructor is assembelled

--- src/scheduling_rlgnn/rllib_agent/__init__.py
+++ src/scheduling_rlgnn/rllib_agent/__init__.py
@@ -4,3 +4,10 @@ from .mlpencoder import MLPEncoder
 from .ppojobshoprlmodule import PPOJobShopRLModule
 from .policyhead import PolicyHead
 from .valuehead import ValueHead
+
+__all__ = [
+    "MLPEncoder",
+    "PPOJobShopRLModule",
+    "PolicyHead",
+    "ValueHead",
+]
