4b837d56630d06a2d53d92b1f394aeb656155a25
NavidCOMSC <amir.navid.rahimi@gmail.com>
2025-10-22 03:04:54 +0100

    main method for running the PPO training and test method added

--- src/scheduling_rlgnn/rllib_agent/_train_environment.py
+++ src/scheduling_rlgnn/rllib_agent/_train_environment.py
@@ -183,3 +183,11 @@ def evaluate_trained_agent(checkpoint_path=None):
     env.close()
     ppo_algorithm.stop()
     ray.shutdown()
+
+    if __name__ == "__main__":
+
+        # First, test the environment
+        test_environment()
+
+        # Then, train the PPO agent
+        train_ppo_node_isolation()
